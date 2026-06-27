fee8d2ebdf18aaee3b50a06b9a4458a371544b82
RafaelCampoTech <1rafael.campo@gmail.com>
2026-06-27 09:57:46 -0300

    Clojure 1.12.0

--- bookmind/project.clj
+++ bookmind/project.clj
@@ -3,13 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
                  [http-kit "2.3.0"]
                  [metosin/muuntaja "0.6.11"]
                  [environ "1.2.0"]
                  [cheshire "6.2.0"]  
                  [metosin/reitit "0.10.1"]
-                 #_[metosin/ring-http-response "0.9.5"]
+                 [metosin/ring-http-response "0.9.5"]
                  [fi.metosin/reitit-openapi "0.10.1"]
                  [metosin/reitit-swagger-ui "0.10.1"]
                  [metosin/jsonista "0.3.13"]
