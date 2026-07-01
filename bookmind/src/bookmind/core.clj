7ae8e45db9113636114176bff40258e3d043db6f
RafaelCampoTech <1rafael.campo@gmail.com>
2026-06-30 21:36:03 -0300

    Refactor server management and logging in core.clj

--- bookmind/src/bookmind/core.clj
+++ bookmind/src/bookmind/core.clj
@@ -2,24 +2,35 @@
   (:gen-class)
   (:require
    [org.httpkit.server :as http]
-   [bookmind.handler :refer [handler]])) 
+   [taoensso.timbre :as t]
+   [bookmind.handler :refer [handler]]))
 
 (defonce server (atom nil))
 
 (defn stop-server []
+  (t/info "Stoping the server")
   (when-let [stop-fn @server]
     (stop-fn :timeout 100)
     (reset! server nil)))
 
-(defn start-server []
-  (reset! server
-          (http/run-server #'handler {:port 8080})))
 
-(defn reset-server []
+(defonce server-settings
+  {:port  8080})
+
+
+(defn start-server
+  []
+  (t/info "Starting the server in port")
+  (reset! server
+          (http/run-server #'handler server-settings)))
+
+(defn reset-server!
+  []
   (stop-server)
   (start-server))
 
- 
+
 (defn -main [& _]
+  (t/info "Main Thread")
   (start-server)
   (println "Listening on http://localhost:8080/docs"))
