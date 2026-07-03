747d94e25149e5f0e1f307073bf93330948fd0a9
RafaelCampoTech <1rafael.campo@gmail.com>
2026-07-03 14:16:20 -0300

    Refactor book handler and model to improve parameter handling and logging

--- bookmind/src/bookmind/services/internet_archive/handler.clj
+++ bookmind/src/bookmind/services/internet_archive/handler.clj
@@ -3,10 +3,16 @@
             [ring.util.http-response :as resp]))
 
 
+(defonce dbg (atom nil))
+
 (defn get-book-handler
-  [{{{:keys [title]} :query} :parameters}]
+  [req]
+  (reset! dbg req)
   (try
-    (let [response (model/search-book-by-title title)]
+    (let [params (-> req
+                    :parameters
+                    :query)
+          response (model/search-book-by-title params)]
       (if (= (:status response) 200)
         (resp/ok (:response response))
         (resp/internal-server-error {:error "Failed to fetch book data from Internet Archive"})))
@@ -14,7 +20,4 @@
     (catch Exception e
       (resp/internal-server-error {:error (.getMessage e)
                                    :msg "An error occurred while processing the request"}))))
-
-
-
-
+ 
--- bookmind/src/bookmind/services/internet_archive/model.clj
+++ bookmind/src/bookmind/services/internet_archive/model.clj
@@ -1,6 +1,7 @@
 (ns bookmind.services.internet-archive.model
   (:require [org.httpkit.client :as http]
-            [bookmind.utils :as utils]))
+            [bookmind.utils :as utils]
+            [taoensso.timbre :as t]))
 
 
 (defonce base-url "https://archive.org/advancedsearch.php")
@@ -8,17 +9,19 @@
 
 (defn fetch-books-by-title
   "Queries Internet Archive API for a given title and returns the results as a map containing the status, body, and headers of the response."
-  [title & {:keys [rows page]}]
+  [{:keys [title limit offset]}]
+  (t/info "Fetching books from Internet Archive for title:" title)
   (let [url (str base-url)
         options {:query-params {"q" title
                                 "fl[]" ["identifier" "title" "description"]
-                                "rows" (or rows 10)
-                                "page" (or page 1)
+                                "rows" (or limit 10)
+                                "page" (or offset 1)
                                 "output" "json"}}]
     (-> @(http/get url options)
         (select-keys [:status :body :headers])
         utils/parse-body-params)))
 
+ 
 
 (defn search-book-by-title
   "Searches for books in the Internet Archive by title and returns a map containing the status and response body."
@@ -26,7 +29,9 @@
   (let [{:keys [status body]} (fetch-books-by-title title)
         response (get body :response)]
     (cond (not= status 200)
-          (throw (ex-info "Error fetching books from Internet Archive" {:status status :body body}))
+
+          (do   (t/error "Error fetching books from Internet Archive for title:" title "Status:" status)
+                (throw (ex-info "Error fetching books from Internet Archive" {:status status :body body})))
 
           :else  {:status status
                   :response response})))
