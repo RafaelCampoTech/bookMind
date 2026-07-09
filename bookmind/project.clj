24f5ac3da04500b47fba6908f19fff21a912d380
RafaelCampoTech <1rafael.campo@gmail.com>
2026-07-09 19:39:40 -0300

    Update jsonista dependency version to 0.3.14

--- bookmind/project.clj
+++ bookmind/project.clj
@@ -13,7 +13,7 @@
                  [metosin/ring-http-response "0.9.5"]
                  [fi.metosin/reitit-openapi "0.10.1"]
                  [metosin/reitit-swagger-ui "0.10.1"]
-                 [metosin/jsonista "0.3.13"]
+                 [metosin/jsonista "0.3.14"]
                  [mount "0.1.24"]
                  [nrepl "1.7.0"]
                  [com.taoensso/timbre "6.8.0"]]
